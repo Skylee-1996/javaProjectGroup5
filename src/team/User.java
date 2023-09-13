7b8920161c0835d7c4c767ba42f67a731bec56f0
DESKTOP-HBSQ8KJ\김수진 <poxz12345@naver.com>
2023-09-13 20:59:46 +0900

    회원정보

--- src/team/User.java
+++ src/team/User.java
@@ -1,49 +1,208 @@
 package team;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class User {
-    private String username;   // 사용자 아이디
-    private String password;   // 사용자 비밀번호 
-    private String name;       // 사용자 이름
-    private int balance;       // 사용자 게임 잔액
-
-    // 생성자
-    public User(String username, String password, String name, int balance) {
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.balance = balance;
+	Scanner scan = new Scanner(System.in);	
+    private HashMap<String, Integer> user1 = new HashMap<>(); //아디 비번
+    private HashMap<String, Integer> user2 = new HashMap<>(); //이름 잔액
+    
+    //메뉴
+    public void menu() {
+    	System.out.println("----user data----");
+    	System.out.println("1.회원 등록 | 2.회원 삭제 | 3.회원 수정 | 4.회원 검색 | 5.자본금 관리 | 6.순위 | 7.종료");
+    	System.out.println("메뉴 입력> ");
+    }
+    
+    //회원등록
+    public void userAdd() {
+    	System.out.println("등록하실 회원ID,비밀번호,회원명,자본금을 입력해주세요.");
+    	System.out.println("회원ID: ");
+    	String id = scan.next();
+    	System.out.println("비밀번호: ");
+    	int password = scan.nextInt();
+    	user1.put(id, password);
+    	System.out.println("회원명: ");
+    	String name = scan.next();
+    	System.out.println("자본금: ");
+    	int balance = scan.nextInt();
+    	user2.put(name, balance);
+    	System.out.println("입력하신 정보로 등록되었습니다.");
+    	System.out.println("메뉴를 선택해주세요.> ");
+    	
+    }
+    
+    //회원삭제
+    public void deleteUser() {
+    	System.out.println("삭제하실 회원ID와 회원명을 입력해주세요.");
+    	System.out.println("회원ID: ");
+    	String id = scan.next();
+    	System.out.println("회원명: ");
+    	String name = scan.next();
+    	for(String tmp : user1.keySet()); {
+    	for(String tmp2 : user2.keySet());{
+    	user1.remove(id);
+    	user2.remove(name);
+    	System.out.println("삭제가 완료되었습니다.");
+    	System.out.println("메뉴를 선택해주세요.> ");
+    }
+    	}
     }
 
-    //getter/setter
-    public String getUsername() {
-        return username;
+    //회원수정
+    public void editUser() {
+        System.out.println("수정하실 회원ID와 회원명을 입력해주세요.");
+	    System.out.println("회원ID: ");
+        String currentId = scan.next();
+        System.out.println("회원명: ");
+        String currentName = scan.next();
+        if (user1.containsKey(currentId) && user2.containsKey(currentName)) {
+            int choice;
+            do {
+                System.out.println("---회원정보 수정 메뉴---");
+                System.out.println("1. ID 수정");
+                System.out.println("2. 비밀번호 수정");
+                System.out.println("3. 회원명 수정");
+                System.out.println("0. 종료");
+                choice = scan.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("새로운 ID를 입력해주세요.");
+                        String newId = scan.next();
+                        if (!user1.containsKey(newId)) {
+                            int password = user1.remove(currentId);
+                            user1.put(newId, password);
+                            System.out.println("입력하신 정보로 수정되었습니다.");
+                        } else {
+                            System.out.println("기존 아이디와 일치하므로 변경하실 수 없습니다.");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("새로운 비밀번호를 입력해주세요.");
+                        int newPassword = scan.nextInt();
+                        user1.put(currentId, newPassword);
+                        System.out.println("입력하신 정보로 수정되었습니다.");
+                        break;
+
+                    case 3:
+                        System.out.println("새로운 회원명을 입력해주세요.");
+                        String newName = scan.next();
+                        if (!user2.containsKey(newName)) {
+                            int balance = user2.remove(currentName); 
+                            user2.put(newName, balance);
+                            System.out.println("입력하신 정보로 수정되었습니다.");
+                        } else {
+                            System.out.println("기존 회원명과 일치하므로 변경하실 수 없습니다.");
+                        }
+                        break;
+                    case 0:
+                        System.out.println("회원수정을 종료합니다."); break;
+
+                    default:
+                        System.out.println("잘못된 메뉴를 입력하셨습니다. 다시 입력해주세요."); break;
+                }
+            } while (choice != 0);
+        } else {
+            System.out.println("회원이 존재하지 않거나 회원정보가 일치하지 않습니다.");
+        }
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    //회원검색 후 출력
+		public void searchUser() {
+		    System.out.println("검색하실 회원ID와 회원명을 입력해주세요.");
+		    System.out.println("회원ID: ");
+		    String id = scan.next();
+		    System.out.println("회원명: ");
+		    String name = scan.next();
+		    if (user1.containsKey(id)) {
+		        System.out.println("회원ID: " + id + ",비밀번호: " + user1.get(id));
+		    } else {
+		        System.out.println("존재하지 않는 회원입니다.");
+		    }
+		    if (user2.containsKey(name)) {
+		        System.out.println("회원명: " + name + ",자본금: " + user2.get(name));
+		    } else {
+		        System.out.println("존재하지 않는 회원입니다.");
+		    }
+		}
+		
+		//자본금-메뉴
+		public void balanceChoice() {
+			System.out.println("입금:1 ,출금:2, 종료:3");
+			int num = scan.nextInt();
+			switch(num) {
+			case 1: depositMoney(); break;
+			case 2: withdrawMoney(); break;
+			case 3: System.out.println("자본금 관리메뉴를 종료합니다."); break;
+			default: System.out.println("잘못된 메뉴입니다. 다시 선택해주세요.");
+			}
+			System.out.println("메뉴를 선택해주세요.");
+		}
 
-    public String getPassword() {
-        return password;
-    }
+		//자본금-입금
+		public void depositMoney() {
+		    System.out.println("회원ID와 회원명을 입력해주세요.");
+		    System.out.println("회원ID: ");
+		    String id = scan.next();
+		    System.out.println("회원명: ");
+		    String name = scan.next();
+		    if (user1.containsKey(id) && user2.containsKey(name)) {
+		        System.out.println("입금하실 금액을 입력해주세요.");
+		        int amount = scan.nextInt();
+		        int currentBalance = user2.get(name);
+		        user2.put(name, currentBalance + amount);
+		        System.out.println("입력하신 " + amount + "원이 성공적으로 입금처리되었습니다.");
+		        System.out.println("현재 자본금: " + user2.get(name));
+		    } else {
+		        System.out.println("회원정보가 일치하지 않거나 없는 회원입니다.");
+		    }
+		}
+		
+		//자본금-출금
+		public void withdrawMoney() {
+		    System.out.println("회원ID와 회원명을 입력해주세요.");
+		    System.out.println("회원ID: ");
+		    String id = scan.next();
+		    System.out.println("회원명: ");
+		    String name = scan.next();
+		    if (user1.containsKey(id) && user2.containsKey(name)) {
+		        System.out.println("출금하실 금액을 입력해주세요.");
+		        int amount = scan.nextInt();
+		        int balance = user2.get(name);
+		        if (balance >= amount) {
+		            user2.put(name, balance - amount);
+		            System.out.println("입력하신 " + amount + "원이 성공적으로 출금처리되었습니다.");
+		            System.out.println("잔액: " + user2.get(name));
+		        } else {
+		            System.out.println("잔액이 부족합니다.");
+		        }
+		    } else {
+		        System.out.println("회원정보가 일치하지 않거나 없는 회원입니다.");
+		    }
+		}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
+	//순위
+		public void rankUser() {
+	        List<Map.Entry<String, Integer>> list = new ArrayList<>(user2.entrySet());
+	        list.sort(new Comparator<Map.Entry<String, Integer>>() {
+	           
+	        	@Override
+	            public int compare(Map.Entry<String, Integer> user1, Map.Entry<String, Integer> user2) {
+	                return user2.getValue().compareTo(user1.getValue());
+	            }
+	        });
+	        
+	        System.out.println("--자본금 순위--");
+	        int rank = 1;
+	        for (Map.Entry<String, Integer> entry : list) {
+	            System.out.println("랭크 " + rank + "위: " + entry.getKey() + " ,잔액: " + entry.getValue());
+	            rank++;
+	        }
+	    }
+	    
 }
